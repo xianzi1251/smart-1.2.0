6b20b63316287f2fb67a350101fe3bcbf5220197
xianzi1251 <332532651@qq.com>
2017-08-18 17:49:23 +0800

    首页底部广告位添加“去商城首页”的跳转

--- src/common/controllers/index.js
+++ src/common/controllers/index.js
@@ -185,6 +185,12 @@ angular.module('app.controllers').controller('indexCtrl', function(
             // 专栏作家列表视图
             nativeTransition.forward();
             $state.go('tabs.authorList');
+        },
+
+        // 去商城首页
+        goMallIndex: function() {
+            nativeTransition.forward();
+            $state.go('tabs.mallIndex');
         }
     });
 
